ea7b61fbf8af6c65c009b3d44e27521087ff411b
Irapuspitasari <irapuspitasari1406@gmail.com>
2023-05-26 23:54:02 +0700

    todolist app

---
+++ src/components/Todos.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo, handleCheckbox } from "../redux/todoapp/actions";
+
+export const Todos = ({ handleEditClick, editFormVisibility }) => {
+  const dispatch = useDispatch();
+
+  const todos = useSelector((state) => state.operationsReducer);
+  return todos.map((todo) => (
+    <div key={todo.id} className="todo-box">
+      <div className="content">
+        {editFormVisibility === false && (
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => dispatch(handleCheckbox(todo.id))}
+          ></input>
+        )}
+        <p
+          style={
+            todo.completed === true
+              ? { textDecoration: "line-through" }
+              : { textDecoration: "none" }
+          }
+        >
+          {todo.todo}
+        </p>
+      </div>
+      <div className="actions-box">
+        {editFormVisibility === false && (
+          <>
+            <span onClick={() => handleEditClick(todo)}>
+              <Icon icon={edit2} />
+            </span>
+            <span onClick={() => dispatch(removeTodo(todo.id))}>
+              <Icon icon={trash} />
+            </span>
+          </>
+        )}
+      </div>
+    </div>
+  ));
+};
